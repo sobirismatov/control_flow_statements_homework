7ec150204a00008f149fe975997b4e2efae4dc6d
sobirismatov <name@uzcoder96@gmail.com>
2022-10-28 14:54:48 +0500

    natija

--- if04.py
+++ if04.py
@@ -11,10 +11,9 @@ def main(a,b,c):
    
     if a>0 and b>0 and c>0:
         return 3
-    if a<0 and b>0 and c>0 or a<0 and b<0 and c>0 or a>0 and b>0 and c<0:
+    if a<0 and b>0 and c>0 or a>0 and b<0 and c>0 or a>0 and b>0 and c<0:
         return 2
     if a<0 and b<0 and c>0 or a<0 and b>0 and c<0 or a>0 and b<0 and c<0:
         return 1
     else:
         return 0
-print(main(-1,-2,-3))
