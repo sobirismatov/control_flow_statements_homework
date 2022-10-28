5180c52dcd294c9ed2759f1ca4bd83133324ae26
sobirismatov <name@uzcoder96@gmail.com>
2022-10-28 15:29:11 +0500

    natija

--- if09.py
+++ if09.py
@@ -9,4 +9,8 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    if (a%2*10+a//10)<=a:
+        return "true"
+    else:
+        return "false"
+print(main(21))
