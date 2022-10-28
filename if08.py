ff8266c5b3bfcb13035120e5bccd112940f7b06b
sobirismatov <name@uzcoder96@gmail.com>
2022-10-28 15:08:05 +0500

    natija

--- if08.py
+++ if08.py
@@ -11,4 +11,11 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a//100!=(1 and 2 and 3 and 4 and 5 and 6 and 7 and 8 and 9) and a%2!=0:
+        return "ikki xonali toq raqam"
+    if a//100!=(1 and 2 and 3 and 4 and 5 and 6 and 7 and 8 and 9) and a%2==0:
+        return "ikki xonali juft raqam"
+    if a//100==(1 and 2 and 3 and 4 and 5 and 6 and 7 and 8 and 9) and a%2!=0:
+       return  "uch xonali toq raqam"
+    if a//100==(1 and 2 and 3 and 4 and 5 and 6 and 7 and 8 and 9) and a%2==0:
+       return  "uch xonali juft raqam"
