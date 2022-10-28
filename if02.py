67f93b0ed59d1932427f0bc4e65cad66d1d3bf94
sobirismatov <name@uzcoder96@gmail.com>
2022-10-28 14:01:47 +0500

    natija

--- if02.py
+++ if02.py
@@ -7,6 +7,6 @@ def main(a):
         a: a increased by 1 if positive, else decreased by 2.
     """
     if a>0:
-        return a-1
-    if a<0 and a==0:
-        return  a+2
+        return a+1
+    else :
+        return  a-2
